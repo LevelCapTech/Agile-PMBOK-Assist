e001d6bcb1453edae0db796c0f7a9f28846c4b65
Yoshio Nishiyama <nishiyama@lvcap.jp>
2026-02-01 00:15:27 +0900

    Update vite.config.ts

--- app/react/web/vite.config.ts
+++ app/react/web/vite.config.ts
@@ -14,7 +14,7 @@ import { restart } from './plugins/restart';
 import { restartEnvFileChange } from './plugins/restartEnvFileChange';
 
 export default defineConfig(({ command }) => ({
-  base: '/Agile-PMBOK-Ops/',
+  base: '/Agile-PMBOK-Assist/',
   // Keep them available via import.meta.env.NEXT_PUBLIC_*
   envPrefix: 'NEXT_PUBLIC_',
   optimizeDeps: {
